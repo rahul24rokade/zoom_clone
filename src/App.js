df6c4cb1252853d6ba4c06fb94e98e2ba31824ef
rahul <rahul24rokade@gmail.com>
2020-10-21 00:56:30 +0530

    yarn delete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import { BrowserRouter, Link, Route} from "react-router-dom"
+import { BrowserRouter, Route} from "react-router-dom"
 import Header from "./zoom/Header"
 import HomePage from "./zoom/Home"
 import MeetingPage from "./zoom/Meeting"
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <header/>
+        
       <Route exact path="/" component={HomePage}/>
       <Route exact path="/meeting/:id" component={MeetingPage}/>
       </div>
