5c5dfac65f0e4eb428a7baa3b56549a102940905
rahul <rahul24rokade@gmail.com>
2020-10-21 14:16:07 +0530

    Updates

---
+++ precache-manifest.57fac8cb8bda984124f9f18e9147048a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe711165df1fc42c03a9ea3d1714ac48",
+    "url": "/zoom_clone/index.html"
+  },
+  {
+    "revision": "d76898db14ef090465cf",
+    "url": "/zoom_clone/static/css/main.3d5c44fc.chunk.css"
+  },
+  {
+    "revision": "be46db7bdd47183a06b7",
+    "url": "/zoom_clone/static/js/2.ccf43f14.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/zoom_clone/static/js/2.ccf43f14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d76898db14ef090465cf",
+    "url": "/zoom_clone/static/js/main.8335c857.chunk.js"
+  },
+  {
+    "revision": "5c62c4525bb12262da17",
+    "url": "/zoom_clone/static/js/runtime-main.b7dc7f21.js"
+  },
+  {
+    "revision": "ca145fade8cdf41447c2640e79d92021",
+    "url": "/zoom_clone/static/media/microphone.ca145fad.png"
+  },
+  {
+    "revision": "39ab82dfc3af0d2bbe7cf79a648a1ed4",
+    "url": "/zoom_clone/static/media/webcam.39ab82df.png"
+  }
+]);
